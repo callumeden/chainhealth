3a05647cf5a98b1cf2c52927130a7d8c4df4c888
Callum Eden <callum.eden15@imperial.ac.uk>
2019-04-23 10:24:22 +0100

    concatting address files into one

--- scripts/prepare_bitcoin_address.sh
+++ scripts/prepare_bitcoin_address.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+echo "Concat of address files into one"
+
+cat ./import/data/test-*/*-address-data-*.csv > ./import/data/sample-address-data.csv
+echo "De duping address file"
+
+#sort -u ./import/data/sample-address-data.csv -o ./import/data/sample-address-data.csv
+
+#echo "Done de-dupe"
